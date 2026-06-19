9058219808df7af686b87eee4a9cf28cf994c6f7
David Hoerl <hoerldavid@gmail.com>
2026-06-19 16:25:38 +0200

    WIP on image2image

--- segmentation/segmentation_utils.py
+++ segmentation/segmentation_utils.py
@@ -1,4 +1,33 @@
 import numpy as np
+import warnings
+
+def scale_intensities(arr, in_range, out_range=(0, 1), clip=False):
+    """
+    Non-clipping (optional) intensity rescaling.
+    Implemented using basic array calculations & selections, should work with both NumPy and PyTorch arrays.
+    """
+
+    # TODO: move to CalmUtils
+
+    in_low, in_high = in_range
+
+    if in_high == in_low:
+        warnings.warn(f"Input range low and high are equal, will result in division-by-zero.")
+
+    # scale to 0-1
+    arr = (arr - in_low) / (in_high - in_low)
+
+    # scale to output range if it is not (0-1)
+    if out_range != (0, 1):
+        out_low, out_high = out_range
+        arr = arr * (out_high - out_low) + out_low
+
+    if clip:
+        arr[arr < out_low] = out_low
+        arr[arr > out_high] = out_high
+
+    return arr
+    
 
 def normalize_by_quantiles(arr, quantiles, clip=False):
     """
@@ -30,14 +59,7 @@ def normalize_by_quantiles(arr, quantiles, clip=False):
     v_low  = np.quantile(flat, q_low)
     v_high = np.quantile(flat, q_high)
 
-    if v_high == v_low:
-        raise ValueError(f"Quantiles {q_low} and {q_high} are identical "
-                         "(value range zero). Cannot normalise.")
-
-    # Linear mapping: (x - v_low) / (v_high - v_low)
-    norm_flat = (flat - v_low) / (v_high - v_low)
-
-    if clip:
-        norm_flat = np.clip(norm_flat, 0, 1)
+    
+    norm_flat = scale_intensities(flat, (v_low, v_high), clip=clip)
 
     return norm_flat.reshape(arr.shape)
